52f299671b82c68569b92b3e7bc445703997c7c0
Rafal <stapinskirafal@gmail.com>
2015-10-04 02:29:58 -0400

    starting search

--- djone/serv/api.py
+++ djone/serv/api.py
@@ -3,7 +3,8 @@ import json
 import twilio.twiml
 from pymongo import MongoClient
 import spotify
-
+from config import Config
+import threading
 urls = (
     "/new_song", "new_song",
 )
@@ -12,19 +13,34 @@ client = MongoClient("127.0.0.1", 27017)
 djone = client.djone
 djs = djone.djs
 
+logged_in_event = threading.Event()
+session = spotify.Session()
+
+def connection_state_listener(session):
+    if session.connection.state is spotify.ConnectionState.LOGGED_IN:
+        logged_in_event.set()
+
+session.on(spotify.SessionEvent.CONNECTION_STATE_UPDATED, connection_state_listener)
+
+session.login(Config.username, Config.password)
+
+while not logged_in_event.wait(0.1):
+    session.process_events()
+
 class new_song:
     def POST(self):
         # web.header("Content-Type", "application/xml")
         web.header("Access-Control-Allow-Origin", "*")
         data = web.input()
-        body = data["Body"]
+        query = data["Body"]
         sender = data["From"]
         dj = data["To"]
 
         if djs.find({"dj": "+17323911722"}):
-
+            search = session.search(query)
+            print search.load()
         else:
-                print "No dj found, let user know"
+            print "No dj found, let user know"
 
 
 if __name__ == "__main__":

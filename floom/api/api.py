b48c7bc5a7f5ef092ed7a3ba46192d2a7f2cdb16
Rafal <stapinskirafal@gmail.com>
2015-10-03 21:27:00 -0400

    started djone server

--- floom/api/api.py
+++ floom/api/api.py
@@ -1,5 +1,18 @@
 import spotify
 from config import Config
+import time
+import threading
 
+logged_in_event = threading.Event()
 session = spotify.Session()
+
+def connection_state_listener(session):
+    if session.connection.state is spotify.ConnectionState.LOGGED_IN:
+        logged_in_event.set()
+
+session.on(spotify.SessionEvent.CONNECTION_STATE_UPDATED, connection_state_listener)
+
 session.login(Config.username, Config.password)
+
+while not logged_in_event.wait(0.1):
+    session.process_events()
